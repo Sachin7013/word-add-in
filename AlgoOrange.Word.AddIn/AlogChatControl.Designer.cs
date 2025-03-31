37ed673a6d90452959d8ccda509a2e111803e03d
Sachin7013 <sachinbfrnd@gmail.com>
2025-03-31 11:51:58 +0530

    first commit

---
+++ AlgoOrange.Word.AddIn/AlogChatControl.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AlgoOrange.Word.AddIn
+{
+    partial class AlogChatControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 680);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(432, 43);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Ask Algo AI";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(3, 390);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(432, 217);
+            this.richTextBox3.TabIndex = 1;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 613);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(432, 61);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(16, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(131, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(148, 21);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "AlgoOrange AI";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(67, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // AlogChatControl
+            // 
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button4);
+            this.Name = "AlogChatControl";
+            this.Size = new System.Drawing.Size(438, 735);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ AlgoOrange.Word.AddIn/AlogChatControl.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using MsWord = Microsoft.Office.Interop.Word;
+
+namespace AlgoOrange.Word.AddIn
+{
+    public partial class AlogChatControl : UserControl
+    {
+        //private const string MessageHistoryFilePath = "ChatHistory.txt";
+        //private Label label3;
+        //private RichTextBox richTextBox1.text;
+        //private const string LogoImagePath = @"C:\AlgoOrange.Excel.AddIn\icon.png"; // Store the image path as a constant
+
+        //public ChatControl()
+        //{
+        //    InitializeComponent();
+        //    LoadMessageHistory();
+        //    LoadLogo(); // Call LoadLogo to load the image when the control is created
+        //}
+
+        //private void InitializeComponent()
+        //{
+
+
+
+        //}
+        private ThisAddIn addIn;
+
+        public AlogChatControl(ThisAddIn addIn)
+        {
+            InitializeComponent();
+            this.addIn = addIn;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            MsWord.Document doc = addIn.Application.ActiveDocument;
+            string wordContent = doc.Content.Text;
+            
+
+            SendXLSDataToAlgoAPI(wordContent, textBox1.Text);
+        }
+
+        private void SendXLSDataToAlgoAPI(string jsonData, string userQuery = null, string chatHistory = null)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://127.0.0.1:8000/");
+
+                    // Wrap the data in an object with a "data" field
+                    // var wrappedData = new { data = jsonData, userQuery = "Hai", chatHistory = "Wow" };
+                    var wrappedData = new { data = JsonConvert.SerializeObject(jsonData), userQuery = userQuery, chatHistory = chatHistory };
+                    string wrappedJsonData = JsonConvert.SerializeObject(wrappedData);
+
+                    StringContent content = new StringContent(wrappedJsonData, Encoding.UTF8, "application/json");
+
+                    // Log the JSON data being sent
+                    Debug.WriteLine("Sending JSON data: " + wrappedJsonData);
+
+
+                    HttpResponseMessage response = client.PostAsync("/excel/query", content).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = response.Content.ReadAsStringAsync().Result;
+
+                        // Parse the JSON response
+                        var jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(result);
+
+                        // Display the "message" field in the textbox
+                        this.Invoke((MethodInvoker)delegate { richTextBox3.Text = jsonResponse.message.ToString(); });
+                    }
+                    else
+                    {
+                        this.Invoke((MethodInvoker)delegate { richTextBox3.Text = $"Error: {response.StatusCode}"; });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    this.Invoke((MethodInvoker)delegate { richTextBox3.Text = $"Exception: {ex.Message}"; });
+                }
+            }
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AlogChatControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
