54a96129ddbd6724c74cb088c4d7ea006803b797
Sachin7013 <sachinbfrnd@gmail.com>
2025-03-31 12:22:27 +0530

    updated

--- AlgoOrange.Word.AddIn/AlogChatControl.Designer.cs
+++ AlgoOrange.Word.AddIn/AlogChatControl.Designer.cs
@@ -34,21 +34,31 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label8 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.SkyBlue;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.Location = new System.Drawing.Point(3, 680);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(432, 43);
             this.button4.TabIndex = 0;
             this.button4.Text = "Ask Algo AI";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click_1);
             // 
             // richTextBox3
             // 
+            this.richTextBox3.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.richTextBox3.Location = new System.Drawing.Point(3, 390);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.Size = new System.Drawing.Size(432, 217);
@@ -66,9 +76,9 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(16, 20);
+            this.pictureBox2.Location = new System.Drawing.Point(16, 19);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.Size = new System.Drawing.Size(78, 50);
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
@@ -76,7 +86,8 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(131, 38);
+            this.label8.ForeColor = System.Drawing.Color.Gray;
+            this.label8.Location = new System.Drawing.Point(100, 36);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(148, 21);
             this.label8.TabIndex = 4;
@@ -84,15 +95,101 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(67, 126);
+            this.button5.BackColor = System.Drawing.Color.SkyBlue;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(44, 112);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(154, 37);
             this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Text = "What is AlgoOrange";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.SkyBlue;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(247, 112);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 37);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Our Add-in";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.SkyBlue;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Location = new System.Drawing.Point(44, 184);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(154, 37);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Analyze Doc";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.SkyBlue;
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Location = new System.Drawing.Point(247, 184);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(154, 37);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Summarize Word";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.SkyBlue;
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.Location = new System.Drawing.Point(44, 260);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(154, 37);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Export to PDF";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.SkyBlue;
+            this.button10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button10.Location = new System.Drawing.Point(247, 260);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(154, 37);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Quick Action";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Lucida Bright", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(22, 352);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(118, 19);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "AI Responce";
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.SkyBlue;
+            this.button11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button11.Location = new System.Drawing.Point(247, 334);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(154, 37);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Copy Responce";
+            this.button11.UseVisualStyleBackColor = false;
             // 
             // AlogChatControl
             // 
+            this.BackColor = System.Drawing.Color.Azure;
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.pictureBox2);
@@ -129,5 +226,12 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button11;
     }
 }
